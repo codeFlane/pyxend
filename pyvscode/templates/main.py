a34a642dec714bb751d31459b0181fea5a46901a
TimurNasirov <nta16022013@gmail.com>
2025-08-03 15:56:56 +0300

    initial commit

---
+++ pyvscode/templates/main.py
@@ -0,0 +1,10 @@
+from pyvscode import Extension
+
+ext = Extension()
+
+# create commands like this:
+# @ext.command('COMMAND_NAME')
+# def COMMAND_NAME(context):
+#     ext.show_modal(context['selcted_text'])
+
+ext.run()
